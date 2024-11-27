3bed636b36dad53fb1ce7bd042dd70aa10225928
Pham Nguyen Khoa <khoapnse183214@fpt.edu.vn>
2024-11-27 15:14:17 +0700

    Tạo controller

--- router/task.router.ts
+++ router/task.router.ts
@@ -3,23 +3,10 @@ const router: Router = Router();
 
 import Task from "../models/task.model";
 
+import * as controller from "../controller/task.controller";
 
+router.get("/", controller.index);
 
-router.get("/", async (req: Request, res: Response) => {
-  const tasks = await Task.find({
-    deleted: false,
-  });
-  res.json(tasks);
-});
-
-router.get("/detail/:id", async (req: Request, res: Response) => {
-  const id: String = req.params.id;
-
-  const task = await Task.findOne({
-    _id: id,
-    deleted: false,
-  });
-  res.json(task);
-});
+router.get("/detail/:id", controller.detail );
 
 export const taskRouter: Router =  router;
